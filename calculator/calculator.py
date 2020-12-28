d072134dfbbcaa451c10961ae2109e64a5711f56
Jonitan <skiba8150@gmail.com>
2020-12-28 12:27:47 +0200

    Version 0.3.0 Finished calculator initial implementation.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,12 +1,10 @@
 import re
 import math
 from statistics import mean
+from typing import Union
 
 
-# TODO: problem with negative number handling - to fix.
-
-
-__token_specification = {
+__TOKEN_SPECIFICATION = {
     'ADDITION': r'\+',
     'SUBTRACTION': r'-',
     'MULTIPLICATION': r'\*',
@@ -19,148 +17,143 @@ __token_specification = {
     'MAX': r'\$',
     'MIN': r'&',
     'LEFT_PARENTHESIS': r'\(',
-    'RIGHT_PARENTHESIS': r'\)'
+    'RIGHT_PARENTHESIS': r'\)',
+    'NUMBER': '\w+',
+    'NUMBER_FLOAT': '\w+\.\w+',
 }
 
+__NUMBER_REGEX = f'{__TOKEN_SPECIFICATION["SUBTRACTION"]}{__TOKEN_SPECIFICATION["NUMBER_FLOAT"]}|' \
+                 f'{__TOKEN_SPECIFICATION["SUBTRACTION"]}{__TOKEN_SPECIFICATION["NUMBER"]}|' \
+                 f'{__TOKEN_SPECIFICATION["NUMBER_FLOAT"]}|' \
+                 f'{__TOKEN_SPECIFICATION["NUMBER"]}'
 
-__tok_regex = '|'.join('%s' % value for key, value in __token_specification.items())
+__OPERATION_HANDLERS = (
+    "__handle_parenthesis",
+    "__handle_factorial",
+    "__handle_negate",
+    "__handle_max_min_average",
+    "__handle_power",
+    "__handle_multiplication_division",
+    "__handle_addition_subtraction",
+)
 
 
-def calculate(expression: str):
+def calculate(expression: str) -> Union[int, float]:
     if type(expression) is not str:
         raise TypeError("Expression input should be of str type.")
 
-    expression = __remove_spaces(expression)
-
-    return __calculate(expression)
+    return __calculate(re.sub("\s", "", expression))
 
 
-def __remove_spaces(expression: str):
-    return re.sub("\s", "", expression)
-
-
-def __calculate(expression: str, result: int = 0):
-
+def __calculate(expression: str) -> Union[int, float]:
     if __is_end_of_calculation(expression):
         return expression
 
-    tmp_result = __handle_parenthesis(expression)
-    if tmp_result is None:
-        tmp_result = __handle_factorial(expression)
+    for func in __OPERATION_HANDLERS:
+        tmp_result = eval(func)(expression)
         if tmp_result is None:
-            tmp_result = __handle_negate(expression)
-            if tmp_result is None:
-                tmp_result = __handle_max_min_average(expression)
-                if tmp_result is None:
-                    tmp_result = __handle_power(expression)
-                    if tmp_result is None:
-                        tmp_result = __handle_multiplication_division(expression)
-                        if tmp_result is None:
-                            tmp_result = __handle_addition_subtraction(expression)
+            continue
+        else:
+            return __calculate(tmp_result)
 
-    return __calculate(tmp_result)
+    raise ValueError(f'Expression: {expression} is not legal!')
 
 
-def __is_end_of_calculation(expression: str):
-    token = re.search(f'^(\w+|\w+\.\w+|-\w+|-\w+\.\w+)$', expression)
-
-    if token is None:
-        return False
-    else:
+def __is_end_of_calculation(expression: str) -> bool:
+    if re.search(f'^({__NUMBER_REGEX})$', expression):
         return True
 
+    return False
 
-def __handle_parenthesis(expression: str):
-    token = re.search(f'{__token_specification["LEFT_PARENTHESIS"]}'
-                      f'[^{__token_specification["LEFT_PARENTHESIS"]}{__token_specification["RIGHT_PARENTHESIS"]}]'
-                      f'*{__token_specification["RIGHT_PARENTHESIS"]}',
+
+def __handle_parenthesis(expression: str) -> str:
+    token = re.search(f'{__TOKEN_SPECIFICATION["LEFT_PARENTHESIS"]}'
+                      f'([^{__TOKEN_SPECIFICATION["LEFT_PARENTHESIS"]}{__TOKEN_SPECIFICATION["RIGHT_PARENTHESIS"]}]*)'
+                      f'{__TOKEN_SPECIFICATION["RIGHT_PARENTHESIS"]}',
                       expression)
 
-    if token is None:
-        return None
-    else:
-        return expression[:token.start():] + str(__calculate(expression[token.start() + 1:token.end() - 1:])) + \
-               expression[token.end()::]
+    if token:
+        return expression[:token.start():] + str(__calculate(token.group(1))) + expression[token.end()::]
+
+    return None
 
 
-def __handle_factorial(expression: str):
-    token = re.search(__token_specification["FACTORIAL"], expression)
+def __handle_factorial(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX}){__TOKEN_SPECIFICATION["FACTORIAL"]}', expression)
 
-    if token is None:
-        return None
-    else:
-        result = math.factorial(int(expression[token.start() - 1:token.start():]))
-        return expression[:token.start() - 1:] + str(math.factorial(int(expression[token.start() - 1:token.start():]))) + \
-               expression[token.end()::]
+    if token:
+        return expression[:token.start():] + str(math.factorial(float(token.group(1)))) + expression[token.end()::]
+
+    return None
 
 
-def __handle_negate(expression: str):
-    token = re.search(__token_specification["NEGATE"], expression)
+def __handle_negate(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX}){__TOKEN_SPECIFICATION["NEGATE"]}', expression)
 
-    if token is None:
-        return None
-    else:
-        return expression[:token.start():] + str(-1 * int(expression[token.end():token.end() + 1:])) + \
-               expression[token.end() + 1::]
+    if token:
+        return expression[:token.start():] + str(-1 * float(token.group(1))) + expression[token.end()::]
+
+    return None
 
 
-def __handle_max_min_average(expression: str):
-    token = re.search(f'(\w+)([{__token_specification["MAX"]}{__token_specification["MIN"]}'
-                      f'{__token_specification["AVERAGE"]}])(\w+)', expression)
+def __handle_max_min_average(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX})'
+                      f'([{__TOKEN_SPECIFICATION["MAX"]}{__TOKEN_SPECIFICATION["MIN"]}'
+                      f'{__TOKEN_SPECIFICATION["AVERAGE"]}])'
+                      f'({__NUMBER_REGEX})',
+                      expression)
 
-    if token is None:
-        return None
-    else:
+    if token:
         if token.group(2) == '$':
-            result = max(int(token.group(1)), int(token.group(3)))
+            result = max(float(token.group(1)), float(token.group(3)))
         elif token.group(2) == '&':
-            result = min(int(token.group(1))), int(token.group(3))
+            result = min(float(token.group(1)), float(token.group(3)))
         else:
-            result = mean([int(token.group(1)), int(token.group(3))])
+            result = mean([float(token.group(1)), float(token.group(3))])
 
-    return expression[:token.start():] + str(result) + expression[token.end()::]
+        return expression[:token.start():] + str(result) + expression[token.end()::]
 
-
-def __handle_power(expression: str):
-    token = re.search(__token_specification["POWER"], expression)
-
-    if token is None:
-        return None
-    else:
-        return expression[:token.start() - 1:] + \
-               str(int(expression[token.start() - 1:token.start():]) ** int(expression[token.end():token.end() + 1:])) \
-               + expression[token.end() + 1::]
+    return None
 
 
-def __handle_multiplication_division(expression: str):
-    token = re.search(f'(\w+)([{__token_specification["MULTIPLICATION"]}{__token_specification["DIVISION"]}])(\w+)', expression)
+def __handle_power(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX}){__TOKEN_SPECIFICATION["POWER"]}({__NUMBER_REGEX})', expression)
 
-    if token is None:
-        return None
-    else:
-        if token.group(2) == '*':
-            result = int(token.group(1)) * int(token.group(3))
-        else:
-            result = int(token.group(1)) / int(token.group(3))
+    if token:
+        return expression[:token.start():] + str(float(token.group(1)) ** float(token.group(2))) + expression[token.end()::]
 
-    return expression[:token.start():] + str(result) + expression[token.end()::]
+    return None
 
 
-def __handle_addition_subtraction(expression: str):
-    token = re.search(f'(\w+)([{__token_specification["ADDITION"]}{__token_specification["SUBTRACTION"]}]+)(\w+)',
+def __handle_multiplication_division(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX})'
+                      f'([{__TOKEN_SPECIFICATION["MULTIPLICATION"]}{__TOKEN_SPECIFICATION["DIVISION"]}])'
+                      f'({__NUMBER_REGEX})',
                       expression)
 
-    if token is None:
-        return None
-    else:
-        if token.group(2) == '+':
-            result = int(token.group(1)) + int(token.group(3))
+    if token:
+        if token.group(2) == '*':
+            result = float(token.group(1)) * float(token.group(3))
         else:
-            result = int(token.group(1)) - int(token.group(3))
+            result = float(token.group(1)) / float(token.group(3))
 
-    return expression[:token.start():] + str(result) + expression[token.end()::]
+        return expression[:token.start():] + str(result) + expression[token.end()::]
+
+    return None
 
 
-exp = "1 * 3 @ 7 - (24 + ((5 - 3) + 2)) + 4 + (3! + ~3 + 7) + 5 ^ 2"
-# exp = "2 + 4 * 4 * 2"
-print(calculate(exp))
+def __handle_addition_subtraction(expression: str) -> str:
+    token = re.search(f'({__NUMBER_REGEX})'
+                      f'([{__TOKEN_SPECIFICATION["ADDITION"]}{__TOKEN_SPECIFICATION["SUBTRACTION"]}])'
+                      f'({__NUMBER_REGEX})',
+                      expression)
+
+    if token:
+        if token.group(2) == '+':
+            result = float(token.group(1)) + float(token.group(3))
+        else:
+            result = float(token.group(1)) - float(token.group(3))
+
+        return expression[:token.start():] + str(result) + expression[token.end()::]
+
+    return None
