549b9c00e7e5d9b54d6aba6fea9f8d63a1e18af3
Jonitan <skiba8150@gmail.com>
2021-01-01 20:37:52 +0200

    Version 0.3.3 Minor fixes according to review.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jonitan-calculator",
-    version="0.3.2",
+    version="0.3.3",
     author="Yonatan Naisteter",
     author_email="skiba8150@gmail.com",
     description="Simple calculator implementation.",
