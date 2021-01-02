f961c92ef35aab78a35d172534676a90772a8381
Jonitan <skiba8150@gmail.com>
2021-01-02 20:08:39 +0200

    Version 0.3.5 Minor fixes according to review.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,18 +6,8 @@ from statistics import mean
 
 _NUMBER_REGEX = r'\d+(?:\.\d+)?'
 _NUMBER_NEGATIVE_OPT_REGEX = f'-?{_NUMBER_REGEX}'
-_LEFT_PARENTHESIS_REGEX = r'(?P<LEFT_PARENTHESIS>\()'
-_RIGHT_PARENTHESIS_REGEX = r'(?P<RIGHT_PARENTHESIS>\))'
-_FACTORIAL_REGEX = r'(?P<FACTORIAL>!)'
-_NEGATE_REGEX = r'(?P<NEGATE>~)'
-_AVERAGE_REGEX = r'(?P<AVERAGE>@)'
-_MIN_REGEX = r'(?P<MIN>\&)'
-_MAX_REGEX = r'(?P<MAX>\$)'
-_POWER_REGEX = r'(?P<POWER>\^)'
-_MULTIPLICATION_REGEX = r'(?P<MULTIPLICATION>\*)'
-_DIVISION_REGEX = r'(?P<DIVISION>/)'
-_ADDITION_REGEX = r'(?P<ADDITION>\+)'
-_SUBTRACTION_REGEX = r'(?P<SUBTRACTION>-)'
+_LEFT_PARENTHESIS_REGEX = r'(?:\()'
+_RIGHT_PARENTHESIS_REGEX = r'(?:\))'
 
 
 def calculate(expression: str) -> Union[int, float]:
@@ -31,9 +21,10 @@ def _calculate(expression: str) -> Union[int, float]:
         return expression
 
     for regex, handler in _TOKEN_SPECIFICATION.items():
-        tmp_result = _handle(expression, regex, handler)
-        if tmp_result is not None:
-            return _calculate(tmp_result)
+        match = re.search(regex, expression)
+        if match:
+            solved_step_expression = re.sub(match.re, str(handler(*match.groups())), expression, 1)
+            return _calculate(solved_step_expression)
 
     raise ValueError(f'Expression: {expression} is not legal!')
 
@@ -46,77 +37,20 @@ def _add_operation_variables(operation: str, is_single_variable: bool = False, i
 def _is_end_of_calculation(expression: str) -> bool:
     return re.fullmatch(f'{_NUMBER_NEGATIVE_OPT_REGEX}', expression) is not None
 
-def _handle(expression: str, regex: str, handler) -> str:
-    token = re.search(regex, expression)
-    if token:
-        return re.sub(token.re, handler(token), expression, 1)
-
-def _handle_two_variable_operation(func):
-    def _inner(token):
-        return str(func(float(token.group("FIRST_NUMBER")), float(token.group("SECOND_NUMBER"))))
-    return _inner
-
-def _handle_single_variable_operation(func):
-    def _inner(token):
-        return str(func(float(token.group("NUMBER"))))
-    return _inner
-
-def _handle_parenthesis(token) -> str:
-    return _calculate(token.group("EXTRACTED_EXPRESSION"))
-
-@_handle_single_variable_operation
-def _handle_factorial(a: float) -> float:
-    return math.factorial(a)
-
-@_handle_single_variable_operation
-def _handle_negate(a: float) -> float:
-    return -1 * a
-
-@_handle_two_variable_operation
-def _handle_average(a: float, b: float) -> float:
-    return mean([a, b])
-
-@_handle_two_variable_operation
-def _handle_min(a: float, b: float) -> float:
-    return min(a, b)
-
-@_handle_two_variable_operation
-def _handle_max(a: float, b: float) -> float:
-    return max(a, b)
-
-@_handle_two_variable_operation
-def _handle_power(a: float, b: float) -> float:
-    return a ** b
-
-@_handle_two_variable_operation
-def _handle_multiplication(a: float, b: float) -> float:
-    return a * b
-
-@_handle_two_variable_operation
-def _handle_division(a: float, b: float) -> float:
-    return a / b
-
-@_handle_two_variable_operation
-def _handle_addition(a: float, b: float) -> float:
-    return a + b
-
-@_handle_two_variable_operation
-def _handle_subtraction(a: float, b: float) -> float:
-    return a - b
-
 
 _TOKEN_SPECIFICATION = {
-    f'{_LEFT_PARENTHESIS_REGEX}(?P<EXTRACTED_EXPRESSION>[^{_LEFT_PARENTHESIS_REGEX}{_RIGHT_PARENTHESIS_REGEX}]*){_RIGHT_PARENTHESIS_REGEX}': _handle_parenthesis,
-    _add_operation_variables(f'{_FACTORIAL_REGEX}', True, False): _handle_factorial,
-    _add_operation_variables(f'{_NEGATE_REGEX}', True, True): _handle_negate,
-    _add_operation_variables(f'{_AVERAGE_REGEX}', False, True): _handle_average,
-    _add_operation_variables(f'{_MIN_REGEX}', False, True): _handle_min,
-    _add_operation_variables(f'{_MAX_REGEX}', False, True): _handle_max,
-    _add_operation_variables(f'{_POWER_REGEX}', False, True): _handle_power,
-    _add_operation_variables(f'{_MULTIPLICATION_REGEX}', False, True): _handle_multiplication,
-    _add_operation_variables(f'{_DIVISION_REGEX}', False, True): _handle_division,
-    _add_operation_variables(f'{_ADDITION_REGEX}', False, True): _handle_addition,
-    _add_operation_variables(f'{_SUBTRACTION_REGEX}', False, True): _handle_subtraction,
+    f'{_LEFT_PARENTHESIS_REGEX}(?P<EXTRACTED_EXPRESSION>[^{_LEFT_PARENTHESIS_REGEX}{_RIGHT_PARENTHESIS_REGEX}]*)'
+    f'{_RIGHT_PARENTHESIS_REGEX}': lambda x: _calculate(x),
+    _add_operation_variables(r'(?:!)', True, False): lambda x: math.factorial(float(x)),
+    _add_operation_variables(r'(?:~)', True, True): lambda x: -1 * float(x),
+    _add_operation_variables(r'(?:@)', False, True): lambda x, y: mean([float(x), float(y)]),
+    _add_operation_variables(r'(?:\&)', False, True): lambda x, y: min(float(x), float(y)),
+    _add_operation_variables(r'(?:\$)', False, True): lambda x, y: max(float(x), float(y)),
+    _add_operation_variables(r'(?:\^)', False, True): lambda x, y: float(x) ** float(y),
+    _add_operation_variables(r'(?:\*)', False, True): lambda x, y: float(x) * float(y),
+    _add_operation_variables(r'(?:/)', False, True): lambda x, y: float(x) / float(y),
+    _add_operation_variables(r'(?:\+)', False, True): lambda x, y: float(x) + float(y),
+    _add_operation_variables(r'(?:-)', False, True): lambda x, y: float(x) - float(y),
 }
 
 exp = "4 + ((((5 + 2! - 3!)))) + ((5 * 3) @ 13)" # 19
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jonitan-calculator",
-    version="0.3.4",
+    version="0.3.5",
     author="Yonatan Naisteter",
     author_email="skiba8150@gmail.com",
     description="Simple calculator implementation.",
